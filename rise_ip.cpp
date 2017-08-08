3b83a3573100649bcde0401ff9e04cde03c9a796
lalit184 <lalitshrinivas1@gmail.com>
2017-08-08 18:02:41 +0530

    error correctio  7/8

--- rise_ip.cpp
+++ rise_ip.cpp
@@ -19,7 +19,7 @@ int main()
 	img=imread("images.jpg", CV_LOAD_IMAGE_COLOR); // reading the rgb image in Mat data struct
 
 	Mat img_bw;
-	img_bw=convert_to_bw(img,img_bw);  // conversion to greayscale
+	cvtColor(img, img_bw,COLOR_RGB2GRAY);  // conversion to greayscale
 /*
 
 note: we may have to either pre process the image or not work with the custom bgr to greyscale or both 
@@ -30,16 +30,16 @@ note: we may have to either pre process the image or not work with the custom bg
     Mat square_grad_x, square_grad_y;   // absoute value of derivative 
 
     /// Gradient  along X direction
-	Sobel( img_bw, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-	/// Gradient Y
-	Sobel( image_bw, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+	Sobel( img_bw, grad_x,CV_16S, 1, 0, 3, int scale=1,int delta=0, BORDER_DEFAULT );
+	/// Gradient  along y direction
+	Sobel( image_bw, grad_y,CV_16S, 0, 1, 3,int scale=1,int delta=0, BORDER_DEFAULT );
 
 	square_grad_y=grad_y.mul(grad_y);
 	square_grad_x=grad_x.mul(grad_x);
 
 	Mat img_edge;
 
-    edge=addWeighted( square_grad_x,1, square_grad_y, 0.5, 0, grad );    // more weight is provided in the horizontal direction for more importance to lane detection
+    addWeighted( square_grad_x,1, square_grad_y, 0.5, 0,edge);    // more weight is provided in the horizontal direction for more importance to lane detection
 
     Mat img_edge_dilated;
     Point anchor=Point(-1,-1);
